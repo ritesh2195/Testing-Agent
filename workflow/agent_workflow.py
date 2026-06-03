1039fd4a794553ee1623b8aae0fa24d7701aa44a
Ritesh Ranjan Mishra <riteshranjanmishra938@gmail.com>
2026-06-03 23:50:48 +0530

    refactored

--- workflow/agent_workflow.py
+++ workflow/agent_workflow.py
@@ -10,15 +10,12 @@ from tools.jira_tool import get_jira_ticket_details
 
 def should_regenerate_test_cases(state: StateModel):
 
-    # Stop regeneration after 2 iterations
     if state.iteration_count >= 2:
         return "automation_agent"
-
-    # If review issues exist → regenerate
+    
     if len(state.review_comments.issues) > 0:
         return "agent"
 
-    # If review passed → generate automation
     return "automation_agent"
 
 
@@ -28,52 +25,29 @@ def build_workflow():
 
     graph = StateGraph(StateModel)
 
-    # Nodes
-    graph.add_node(
-        "agent",
-        generate_test_case_generation_agent
-    )
+    graph.add_node("agent", generate_test_case_generation_agent)
 
-    graph.add_node(
-        "tools",
-        ToolNode(tools)
-    )
+    graph.add_node("tools", ToolNode(tools))
 
-    graph.add_node(
-        "review_agent",
-        review_test_case_agent
-    )
+    graph.add_node("review_agent", review_test_case_agent)
 
-    graph.add_node(
-        "automation_agent",
-        create_automation_script_agent
-    )
+    graph.add_node("automation_agent",create_automation_script_agent)
 
-    # Start
     graph.add_edge(START, "agent")
 
-    # Tool handling
-    graph.add_conditional_edges(
-        "agent",
-        tools_condition
-    )
+    graph.add_conditional_edges("agent",tools_condition)
 
     graph.add_edge("tools", "agent")
 
-    # Review flow
     graph.add_edge("agent", "review_agent")
 
-    # Decide regenerate or automate
-    graph.add_conditional_edges(
-        "review_agent",
-        should_regenerate_test_cases,
+    graph.add_conditional_edges("review_agent", should_regenerate_test_cases,
         {
             "agent": "agent",
             "automation_agent": "automation_agent"
         }
     )
 
-    # Final step
     graph.add_edge("automation_agent", END)
 
     return graph.compile()
